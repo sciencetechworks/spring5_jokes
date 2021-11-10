2ec827bb7e5ae5f1fb05f44687b20477b055f426
ScienceTechWorks <sciencetechworks@gmail.com>
2021-11-10 15:08:25 +0100

    Create Spring Web Project.

---
+++ src/main/java/com/stw/demo/Spring5JokesApplication.java
@@ -0,0 +1,13 @@
+package com.stw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stw/demo/Spring5JokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stw.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
